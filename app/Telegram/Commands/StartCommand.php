351e009aa618ff2b951f2c62d8903c498d96d3d8
sevdot <sevdot8@gmail.com>
2024-03-31 18:47:39 +0800

    first commit

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -10,6 +10,7 @@ class StartCommand extends Command
     protected string $description = 'Start Command to get you started';
     public function handle()
     {
+
         $this->replyWithMessage([
             'text' => 'Hey, there! Welcome to our bot!',
         ]);
